700e8f08448fa591d28f1cdf17c3947d7123f58d
doyeong <kdoyeong1@gmail.com>
2026-03-12 13:31:33 +0900

    ✨ Feat: 08.javascript module

---
+++ es6/message.js
@@ -0,0 +1,8 @@
+//기본 내보내기
+const message = () => {
+    const name = "홍길동";
+    const age = 22;
+    return name + '은 ' + age + '세 입니다.';
+}
+
+export default message;
---
+++ es6/person.js
@@ -0,0 +1,8 @@
+//명명된 내보내기 - 개별적
+// export const name = "홍길동"
+// export const age = 22
+
+//-한번에 
+const name = "홍길동"
+const age = 22
+export {name, age}
