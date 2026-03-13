e89afc393a2465f1f61ee0c8ab49efff8ba5f675
doyeong <kdoyeong1@gmail.com>
2026-03-13 16:45:35 +0900

    ✨ Feat: 02.react_JSX Expression

--- my-first-react/src/main.jsx
+++ my-first-react/src/main.jsx
@@ -2,15 +2,20 @@
 //import {createRoot, createElement} from 'react-dom/client';
 import {createRoot} from 'react-dom/client';
 
-function Intro(){
-  return(
-    <>
-      <h1>내 소개</h1>
-      <p>저는 홍길동입니다.</p>
-    </>
-  )
+function Human(){
+  const kildong ={
+  name : "홍길동",
+  address : "서울",
+  height : "180cm"
+}
+return(
+  <>
+    <h1>저는 { kildong.name } 입니다. </h1>
+    <p>{ kildong.address }에 살고 있고요, 키는  { kildong.height } 입니다. </p>
+  </>
+);
 }
 
 createRoot(document.getElementById('root')).render(
-  <Intro/>
+  <Human />
 );
