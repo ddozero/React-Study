46a102d4393498cac083cd70cf89b49281f33742
doyeong <kdoyeong1@gmail.com>
2026-03-14 16:06:51 +0900

    ✨ Feat: 05.JSX React Component

--- my-first-react/src/main.jsx
+++ my-first-react/src/main.jsx
@@ -1,16 +1,31 @@
-//import React from "react";
-//import {createRoot, createElement} from 'react-dom/client';
-import {createRoot} from 'react-dom/client';
+import {createRoot} from 'react-dom/client'
+import Flower from './Flower.jsx';
 
-function Human(){
-  
-const age = 21;
+// function Flower(props){ 
+//   return(
+//     <p>저는 {props.color} 꽃입니다.</p>
+//   )
+// }
 
-return( //여기다가는 if문을 쓸 수는 없음
-  <p>당신은 {(age)>=20? "성인" : "미성년자"} 입니다.</p>
-);
+function Garden(){
+  return(
+    <>
+      <p>저는 정원입니다.</p>
+      <Flower />
+    </>
+  )
+}
+
+function Background(){
+  return(
+    <>
+      <p>저는 하늘입니다.</p>
+      <Flower />
+      <Garden />
+    </>
+  )
 }
 
 createRoot(document.getElementById('root')).render(
-  <Human />
-);
+  <Background />
+)
