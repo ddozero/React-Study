63cc973944ed0064375b9ee29b491513663254c7
doyeong <kdoyeong1@gmail.com>
2026-04-02 14:06:45 +0900

    ✨ Feat: 10.JSX React Conditional Rendering

--- my-first-react/src/main.jsx
+++ my-first-react/src/main.jsx
@@ -1,16 +1,26 @@
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-function App(){
-  const sayHi = (name, e) => {
-    alert(`${name}님, 안녕하세요`);
-    alert(`발생한 이벤트는 ${e.type}`)
-  }
-
+function AdminPanel(){
   return(
-    <button onDoubleClick={(event) => sayHi("홍길동", event)}>인사 버튼</button>
+    <p>관리자 전용 페이지입니다</p>
+  );
+}
+
+function UserPanel(){
+  return(
+    <p>일반 사용자 페이지입니다</p>
   )
 }
 
+function App({isAdmin}){
+
+  return (
+    <>
+      {isAdmin ? <AdminPanel/> : <UserPanel/>}
+    </>
+  );
+}
+
 createRoot(document.getElementById('root')).render(
-  <App/>
-)
+  <App isAdmin={false}/>
+);
