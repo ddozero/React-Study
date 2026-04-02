8665731d4b26ead747949f1812595b3b2a4249ca
doyeong <kdoyeong1@gmail.com>
2026-04-02 14:31:53 +0900

    ✨ Feat: 11.JSX React list

--- my-first-react/src/main.jsx
+++ my-first-react/src/main.jsx
@@ -1,26 +1,20 @@
-import { createRoot } from "react-dom/client";
+import {createRoot} from 'react-dom/client';
 
-function AdminPanel(){
-  return(
-    <p>관리자 전용 페이지입니다</p>
-  );
-}
+function Flowers(){
+  const flowers = ['장미', '매화', '국화'];
 
-function UserPanel(){
   return(
-    <p>일반 사용자 페이지입니다</p>
+    <>
+      <ul>
+        {flowers.map(
+          (flower,index) => <li key ={index}> 나는 {flower} 입니다.</li>
+        )}
+      </ul>
+    
+    </>
   )
 }
 
-function App({isAdmin}){
-
-  return (
-    <>
-      {isAdmin ? <AdminPanel/> : <UserPanel/>}
-    </>
-  );
-}
-
 createRoot(document.getElementById('root')).render(
-  <App isAdmin={false}/>
-);
+  <Flowers />
+)
