f333e90aa393f662d08eac4353577fee115b106d
doyeong <kdoyeong1@gmail.com>
2026-04-04 16:11:28 +0900

    ✨ Feat: 13.React Form Submit

--- my-first-react/src/main.jsx
+++ my-first-react/src/main.jsx
@@ -2,18 +2,22 @@ import { useState } from "react"; //리엑트에서 useState훅을 가져옴
 import { createRoot } from "react-dom/client";
 
 function App(){
+  const [name, setName] = useState("");
 
-  const [name, setName] = useState("홍길동"); //초기값셋팅
-
-  function handleChange(e){
+  function handleChange(e) {
     setName(e.target.value);
+  }
+  
+  function handleSubmit(e){
+    e.preventDefault(); 
+    alert(name);
   }
 
   return(
-    <form>
-      <label>이름입력 :</label>
-      <input type="text" value={name} onChange={handleChange}></input>
-      <p>입력한 값 : {name}</p>
+    <form onSubmit={handleSubmit}>
+      <label>이름입력:</label>
+      <input type="text" value={name} onChange={handleChange}/>
+      <input type="submit"/>
     </form>
   )
 }
