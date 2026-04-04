b94270760a2a715f28c8dcbf6c6e113c775a5419
doyeong <kdoyeong1@gmail.com>
2026-04-04 16:16:58 +0900

    ✨ Feat: 14.React From - textarea

--- my-first-react/src/main.jsx
+++ my-first-react/src/main.jsx
@@ -2,22 +2,19 @@ import { useState } from "react"; //리엑트에서 useState훅을 가져옴
 import { createRoot } from "react-dom/client";
 
 function App(){
-  const [name, setName] = useState("");
+  const [myText, setMyText] = useState("");
 
   function handleChange(e) {
-    setName(e.target.value);
+    setMyText(e.target.value);
   }
   
-  function handleSubmit(e){
-    e.preventDefault(); 
-    alert(name);
-  }
-
   return(
-    <form onSubmit={handleSubmit}>
-      <label>이름입력:</label>
-      <input type="text" value={name} onChange={handleChange}/>
-      <input type="submit"/>
+    <form>
+      <label>값을 입력 : </label>
+      <textarea value={myText} onChange={handleChange}>
+        
+      </textarea>
+      <p>입력 텍스트 : {myText} </p>
     </form>
   )
 }
